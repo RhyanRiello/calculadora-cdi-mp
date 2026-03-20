f057a45b6339720b5f2f98dd4ba2b0f1faeac983
RhyanRiello <rhyancamargo785@gmail.com>
2026-03-20 13:11:57 -0300

    Implement CDI fetching and calculation functionality
    Added a function to fetch the CDI value from the BCB API and calculate the annual CDI rate.

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 document.getElementById("btnCalcular").addEventListener("click", calcular)
 
+// BUSCAR CDI AUTOMATICAMENTE NO BACEN
+async function pegarCDI(){
+
+let url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.12/dados/ultimos/1?formato=json"
+
+try{
+
+let resposta = await fetch(url)
+let dados = await resposta.json()
+
+let cdiDiario = parseFloat(dados[0].valor)
+
+// converter para anual aproximado
+let cdiAnual = (Math.pow(1 + (cdiDiario/100), 252) - 1) * 100
+
+document.getElementById("cdi").value = cdiAnual.toFixed(2)
+
+}catch(erro){
+
+console.log("Erro ao buscar CDI", erro)
+
+}
+
+}
+
+// roda quando abrir o site
+pegarCDI()
+
+
+
 function calcular(){
 
 let valor = parseFloat(document.getElementById("valor").value)
